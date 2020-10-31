b62251964ae0a372392849d7736d72090a4bdc71
vatsal <vatsal30112001@gmail.com>
2020-10-31 11:21:14 +0530

    crud operation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyparser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const uri = "mongodb://localhost:hari/narnarayan";
 mongoose.connect(uri, {
@@ -8,15 +8,30 @@ mongoose.connect(uri, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-app.use(bodyparser.json());
-const cors = require("cors");
-app.use(express.urlencoded());
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 const schema = mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
 });
 const model = mongoose.model("krishn", schema);
-app.use(cors());
+
+//home page
+
+app.get("/hari", (req, res) => {
+  res.send("jay narayan");
+});
+app.get("/read", (req, res) => {
+  res.render(app.use(__dirname, "read.js"));
+});
+
+//saving data to mongodb
+
 app.post("/hari", (req, res) => {
   console.log(req.body);
   const data = new model(req.body);
@@ -30,4 +45,78 @@ app.post("/hari", (req, res) => {
       console.log(err);
     });
 });
+
+//finding all data
+
+app.get("/hari/find", (req, res) => {
+  model
+    .find()
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//finding particular data using id
+
+// app.get('/hari/find/:id',(req,res)=>{
+//   model.findById(req.params.id)
+//   .then(data=>res.json(data))
+//   .catch(console.log('something went wrong in finding'))
+// })
+
+//updating particular data using id
+
+// app.post('/hari/update/:_id',(req,res)=>{
+//   model.findByIdAndUpdate(req.params,{name:req.body.name,phone:req.body.phone},(err,docs)=>{
+//     if(err){console.log(err)}
+//     else{console.log(docs)}
+//   })
+//   .catch(err=>res.json(err))
+// })
+
+//deleting particular data using id
+
+// app.get('/hari/delete/:id',(req,res)=>{
+//   model.findByIdAndDelete(req.params.id)
+//   .then(()=>{res.json('deleted')})
+//   .catch(console.log('something went wrong in deleting'))
+// })
+
+//finding particular data using name
+
+app.get("/hari/find/:name", (req, res) => {
+  const query = { name: req.params.name };
+  console.log(req.params.name);
+  const projection = {
+    name: 1,
+    phone: 1,
+    _id: 0,
+  };
+  model
+    .findOne(query, projection)
+    .then((data) => res.send(data), console.log(query, projection))
+    .catch((err) => console.log(err));
+});
+
+//updating particular data using name
+
+app.post("/hari/update/:name", (req, res) => {
+  const query = { name: req.params.name };
+  model
+    .findOneAndUpdate(query, req.body)
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
+});
+
+//deleting particular data using name
+
+app.get("/hari/delete/:name", (req, res) => {
+  const query = { name: req.params.name };
+  model
+    .findOneAndDelete(query)
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
+});
+
 app.listen(80);
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,6 +4,15 @@ import axios from "axios";
 const Contact = () => {
   const [name, setname] = useState("");
   const [name2, setname2] = useState("");
+  const [name4, setname4] = useState("");
+  const [name5, setname5] = useState("");
+  const [name6, setname6] = useState("");
+  const [name7, setname7] = useState({
+    name2: "",
+    phone2: "",
+  });
+  const [name8, setname8] = useState("");
+
   const change = (e) => {
     setname(e.target.value);
   };
@@ -26,10 +35,56 @@ const Contact = () => {
       });
     console.log(data);
   }
+  const click = () => {
+    axios
+      .get(`http://localhost/hari/find/${name5}`)
+      .then((data) => {
+        setname6(JSON.stringify(data.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const getdata = (e) => {
+    setname5(e.target.value);
+  };
+  const deleteinput = (e) => {
+    setname4(e.target.value);
+    console.log(name4);
+  };
+
+  const deleting = () => {
+    axios
+      .get(`http://localhost/hari/delete/${name4}`)
+      .then(console.log("deleted"));
+  };
+  const change2 = (e) => {
+    const { name, value } = e.target;
+    setname7((olddata) => {
+      return {
+        ...olddata,
+        [name]: value,
+      };
+    });
+  };
+  const findby = (e) => {
+    setname8(e.target.value);
+  };
+  const data = {
+    name: name7.name2,
+    phone: name7.phone2,
+  };
+  const update = () => {
+    axios
+      .post(`http://localhost/hari/update/${name8}`, data)
+      .then(console.log("updadted"))
+      .catch((err) => console.log(err));
+  };
   return (
     <>
       <h1>narnarayan dev</h1>
       <form onSubmit={submit}>
+        <h1>creating</h1>
         name: <input onChange={change} name="name"></input>
         <br />
         phone:<input onChange={phone} name="phone"></input>
@@ -37,6 +92,27 @@ const Contact = () => {
         <br />
         <button type="submit">submit</button>
       </form>
+      <h1>reading</h1>
+      <input onChange={getdata} value={name5}></input>
+      <br />
+      <p>{name6}</p>
+      <button type="button" onClick={click}>
+        getdata
+      </button>
+      <br />
+      <h1>updating</h1>
+      <input onChange={findby} value={name8}></input>
+      <br />
+      name: <input onChange={change2} name="name2"></input>
+      <br />
+      phone:<input onChange={change2} name="phone2"></input>
+      <br />
+      <button onClick={update}>update</button>
+      <br />
+      <h1>deleting</h1>
+      <input onChange={deleteinput} value={name4}></input>
+      <br />
+      <button onClick={deleting}>delte</button>
     </>
   );
 };
